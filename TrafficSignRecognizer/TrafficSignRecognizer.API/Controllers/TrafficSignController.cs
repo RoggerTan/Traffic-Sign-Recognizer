2d05c8b10fc0bcb4fdd368001e4392de03ae48c2
Rogger Tan <leminhtanvatc@yahoo.com.vn>
2018-10-01 01:12:29 +0700

    Done front-end and API structure

--- TrafficSignRecognizer/TrafficSignRecognizer.API/Controllers/TrafficSignController.cs
+++ TrafficSignRecognizer/TrafficSignRecognizer.API/Controllers/TrafficSignController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Drawing;
 
 namespace TrafficSignRecognizer.API.Controllers
 {
@@ -10,14 +13,16 @@ namespace TrafficSignRecognizer.API.Controllers
     [ApiController]
     public class TrafficSignController : ControllerBase
     {
-        [HttpGet("get/{id}")]
-        public ActionResult<string> Get(string id)
+        [HttpPost("get")]
+        public IActionResult Get([FromBody]dynamic img)
         {
+            if (img == null) return NoContent();
+            var imgStream = new Bitmap(new MemoryStream(Convert.FromBase64String(img["base64"].Value)));
             return new JsonResult(new {
                 ImgUrl = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host}/jpg/example.jpg",
                 Name = "CẤM DỪNG XE VÀ ĐỖ",
                 Type = "Biển báo cấm",
-                Id = id,
+                Id = "123",
                 Description = "Để báo nơi cấm dừng xe và đỗ xe. Biển có hiệu lực cấm tất cả các loại xe cơ giới dừng và đỗ lại ở..."
             });
         }
