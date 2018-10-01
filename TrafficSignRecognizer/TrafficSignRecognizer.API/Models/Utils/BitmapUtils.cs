0b87fe8aa342490654613083cbcd1b1c03a0d2bf
Rogger Tan <leminhtanvatc@yahoo.com.vn>
2018-10-01 10:37:17 +0700

    Init Utils folder and BitmapUtils

---
+++ TrafficSignRecognizer/TrafficSignRecognizer.API/Models/Utils/BitmapUtils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TrafficSignRecognizer.API.Models.Utils
+{
+    public static class BitmapUtils
+    {
+        public static int[][] AsMatrix(this Image image, int divider = 1)
+        {
+            
+        }
+    }
+}
